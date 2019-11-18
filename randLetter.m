348325fc6dedb9bea6bc671414fe421d057d96b6
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2019-11-18 14:59:59 -0800

    Can now be called without any arguments
    Function crashes if you input u or l without quotation marks.
    
    I <3 gnu octave reeeeeeeeeeeeeeeeeeeeee

--- randLetter.m
+++ randLetter.m
@@ -3,18 +3,21 @@ function resp = randLetter(entra)
 %--   randLetter()    : returns a random upper or lower case letter
 %--   randLetter('u') : Returns a random upper case letter.
 %--   randLetter('l') : Returns a random lower case letter.
-
-    switch entra
-        case 'l'
-            resp = char(randi([97,120]));
-        case 'u'
-            
-        otherwise
-            _tempor = randi([1,2]);
-            if _tempor == 1
+    if(nargin<1)
+        _tempor = randi([1,2]);
+                if _tempor == 1
+                    resp = char(randi([97,120]));
+                elseif _tempor == 2
+                    resp = char(randi([65,90]));
+                endif
+    else
+        switch entra
+            case 'l'
                 resp = char(randi([97,120]));
-            elseif _tempor == 2
-                resp = char(randi([65,90]));
-            endif
-    endswitch
+            case 'u'
+                resp = char(randi([65,90]));           
+            otherwise
+                disp('You need to input "l" for lowercase letters and "u" for uppercase letters.');
+        end
+    endif
 endfunction
